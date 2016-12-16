7908724a5224c8845f69a9ffde5c5941cfa0ab32
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2016-12-16 15:58:19 -0700

    fixed typo

--- api_calls/parentalrel-api.js
+++ api_calls/parentalrel-api.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 module.exports = function(app, ParentalRelModel) {
   app.get('/api/v2/parentalrels', auth.isAuthenticated, function(req, res) {
     console.log("in get parentalrels");
-    var user = req.decodfed._doc.userName;
+    var user = req.decoded._doc.userName;
     ParentalRelModel.find(
       {
         user_id: user
