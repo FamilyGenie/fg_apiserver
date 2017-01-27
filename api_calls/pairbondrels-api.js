766f6c5f1b74bb033ed1b9dead7c0b9d8f54cb73
joshuaforman <joshuajforman@gmail.com>
2017-01-27 15:23:26 -0700

    modified pairbondrel/delete, so that you can pass it the field and value to delete. this way, we can call it to delete all records where personOne_id is a given value. this is to support a call to delete all records related to a person, before deleting that person

--- api_calls/pairbondrels-api.js
+++ api_calls/pairbondrels-api.js
@@ -50,13 +50,44 @@ module.exports = function(app, PairBondRelModel) {
     );
   })
 
-  app.post('/api/v2/pairbondrel/delete', auth.isAuthenticated, function(req, res) {
+  app.post('/api/v2/pairbondrel/deleteold', auth.isAuthenticated, function(req, res) {
     winston.log(logLevel, date + ": in pairbondrel delete");
     var user = req.decoded._doc.userName;
     var _id = req.body.object._id;
     PairBondRelModel.remove(
       {
         _id: _id,
+        user_id: user
+      },
+      function(err, data){
+        if(err) {
+          res.status(500).send("Error deleting pairBondRel" + err);
+          return;
+        }
+        PairBondRelModel.find(
+          {
+            user_id: user
+          }, // filter object - empty filter catches everything
+          function(err, data) {
+            if(err) {
+              res.status(500);
+              res.send("Error getting all pairBondRels after delete" + err);
+              return;
+            }
+            res.status(200).send(JSON.stringify(data));
+          }
+        );
+      }
+    );
+  });
+
+  // changed this so that you need to pass an object in the body. That object needs a 'field' and a 'value'. This way we can delete records not just based on the _id, but also call and remove all records for a certain personOne_id or personTwo_id, etc... Made this mostly so that when we delete a person, we can remove all the pairbonds related to them
+  app.post('/api/v2/pairbondrel/delete', auth.isAuthenticated, function(req, res) {
+    winston.log(logLevel, date + ": in pairbondrel deleteperson");
+    var user = req.decoded._doc.userName;
+    PairBondRelModel.remove(
+      {
+        [req.body.object.field]: req.body.object.value,
         user_id: user
       },
       function(err, data){
