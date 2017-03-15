59fb37e12bf21d20647514fa775a81a7ec2acd4d
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2017-03-15 10:53:52 -0600

    added clear-saved-records api

--- functions/import-script.js
+++ functions/import-script.js
@@ -10,7 +10,10 @@ var date = new Date();
 
 module.exports = function(app, PersonModel, StagedPersonModel, EventsModel, StagedEventsModel, ParentalRelModel, StagedParentalRelModel, PairBondRelModel, StagedPairBondRelModel) {
 
-  app.post('/api/v2/autoimport', auth.isAuthenticated, function(req, res) {
+  // for each of these functions we are passing in the required model information, as well as the `res` and the `user`. This allows for responses to be sent from within the scripts.
+   
+  // auto-import of people and events. Events are imported for each person, called within the function.
+  app.post('/api/v2/autoimportpeople', auth.isAuthenticated, function(req, res) {
     winston.log(logLevel, date + ': in import-scripts');
 
     require('./person-import')(res, PersonModel, StagedPersonModel, EventsModel, StagedEventsModel)
